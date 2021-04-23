4fbf504879c821888d17884456ebce9f21b85670
Veronica <veronica.gudova@gmail.com>
2021-04-23 10:09:02 -0700

    added redux, implemented user reducer to header and app components

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -54,7 +54,7 @@ class SignIn extends React.Component {
                     required/>
                     <div className='buttons'>
                         <CustomButton type='submit'>SIGN IN</CustomButton>
-                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>
+                        <CustomButton type='button' onClick={ signInWithGoogle } isGoogleSignIn>
                             SIGN IN WITH GOOGLE
                         </CustomButton>
                     </div>
